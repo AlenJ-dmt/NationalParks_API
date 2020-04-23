902bffefc87c1d7cb147b4cd3646cd2cb6ebb047
Alen Diaz <noricual@gmail.com>
2020-04-22 19:00:49 -0500

    search_fixed

--- index.js
+++ index.js
@@ -1,60 +1,68 @@
-"use strict";
-
-const key = 'kUpVdjJwJ2pAX6aj4OpfCnVBgnEFwky5Pg3WNIsZ'
-const baseUrl = 'https://developer.nps.gov/api/v1/parks?'
 
 
-function formatParams(params) {
-    const formatedParams = Object.keys(params)
-        .map(key => `${key}=${params[key]}`)
-    return formatedParams.join('&');
+const apiKey= 'ElZ53b1hGwbuwAs7cFM5PP4KuAnjJoFg5V5I2Uia';
+
+const searchUrl= 'https://developer.nps.gov/api/v1/parks/';
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${key}=${params[key]}`)
+  return queryItems.join('&');
 }
 
-function getParks() {
-
-    const statesInput = $('#park-Search').val().trim().toString();
-    let qty = $('#qty').val().trim().toString();
-
-    if(qty === ''){
-        qty = '10'
-    }
-
-    const states = statesInput.split(', ')
-    const params = {
-        limit: qty,
-        api_key: key
-    }
-
-    for (let i = 0; i < states.length; i++) {
-        params.q = states[i]
-        const parameter = formatParams(params)
-        const url = baseUrl + parameter;
-
-        fetch(url)
-            .then((response) => {
-                if (response.ok) {
-                    console.log(response)
-                    return response.json();
-                }
-                throw new Error(response.statusText);
-            })
-            .then(jsonResponse => displayresults(jsonResponse.data))
-    }
+function displayResults(responseJson){
+  console.log(responseJson);
+  $("#js-search-results").empty();
+  for (let i=0; i < responseJson.data.length; i++){
+    $('#js-search-results').append(
+      `<li><h3>${responseJson.data[i].name}</h3>
+      <p>${responseJson.data[i].description}</p>
+      <a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a>
+      </li>`
+    )
+  }
+$('.results').removeClass('hidden');
 }
 
-function displayresults(results) {
-    $(".loader").css("display", "none");
-    for (let i = 0; i < results.length; i++) {
-        $('#results').append(`<li><h2>${results[i].name}</h2>
-        <p>${results[i].description}</p>
-        <p><a href='${results[i].url}'>${results[i].url}</a></p>
-        </li> `)
-    }
+function getParksList(whichStates, maxResults=10){
+  const params= {
+    api_key: apiKey,
+    stateCode: whichStates,
+    limit: maxResults,
+  };
+  const queryString = formatQueryParams(params);
+  const url = searchUrl + '?' + queryString;
+    console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => {
+      if (responseJson.total == "0"){
+        $('#js-error-message').text(`We couldn't find you any parks. Try typing something else`);
+      }
+      else {displayResults(responseJson)};
+    })
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
 }
 
-function watchForm(ev) {
-    ev.preventDefault()
-    $('#results').empty()
-    $(".loader").css("display", "block");
-    getParks()
+function watchForm() {
+  $('form').submit(event => {
+    console.log('hi');
+    event.preventDefault();
+    const chosenStates = $('#js-search-states').val();
+    const whichStates= chosenStates.replace(/\s+/g, '&stateCode=');
+    //getStatesString(whichStates);
+    console.log(whichStates);
+    const maxResults = $('#js-max-results').val();
+    getParksList(whichStates, maxResults);
+  });
 }
+
+$(watchForm);
